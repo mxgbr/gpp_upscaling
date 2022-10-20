eb23af77cc741ae4f2c2a892621159facdeb963d
Max Gaber <maxgaber@n0003.testbed0>
2022-10-20 06:42:57 -0700

    Blueprint for param files

---
+++ parameters/dummy.py
@@ -0,0 +1,15 @@
+# Model: RF (Random Search)
+# Expl. variables: RS_min
+
+params = {
+    'name': '',
+    'desc': '',
+    'n_folds_cv': 5,
+    'n_folds_tuning': 5,
+    'random_state': None,
+    'variable_set': '',
+    'model': '',
+    'model_params': {
+
+    }
+}
