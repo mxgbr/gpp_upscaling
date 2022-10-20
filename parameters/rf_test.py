f6f6e83dd7fa5e4dbf4601b6d10f1c112cf8f825
Max Gaber <maxgaber@n0003.testbed0>
2022-10-20 05:09:22 -0700

    rf test parameters

---
+++ parameters/rf_test.py
@@ -0,0 +1,13 @@
+params = {
+    'name': 'RF-RS_min',
+    'desc': 'Random Forest (Random Search, default parameters) with RS_min data set',
+    'n_folds_cv': 5,
+    'n_folds_tuning': 5,
+    'random_state': None,
+    'n_cpus': 1,
+    'variable_set': 'rs_min',
+    'model': 'random_forest',
+    'model_params': {
+
+    }
+}
