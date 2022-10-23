e107fcf10eafb7b4cdb4ac72f175d6b88e1dac03
Max Gaber <maxgaber@n0003.testbed0>
2022-10-23 10:47:29 -0700

    Adapted RF settings

--- parameters/rf_rs_min.py
+++ parameters/rf_rs_min.py
@@ -6,7 +6,7 @@ params = {
     'desc': 'Random Forest (Random Search, default parameters) with RS_min data set',
     'n_folds_cv': 5,
     'n_folds_tuning': 5,
-    'random_state': None,
+    'random_state': 26,
     'variable_set': 'rs_min',
     'model': 'random_forest',
     'model_params': {
@@ -15,7 +15,7 @@ params = {
         'min_samples_leaf': [2, 5, 10],
         'min_samples_split': [4, 8, 12, 16],
         'max_depth': [5, 10, 20, None],
-        'n_iter': 50,
+        'n_iter': 150,
         'n_cpus': 20,
         'scoring': 'neg_root_mean_squared_error'
     }
