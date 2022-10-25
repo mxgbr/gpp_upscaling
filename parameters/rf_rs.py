0557adacf049dd77fb953a2efbd290040e98a640
Max Gaber <maxgaber@n0003.testbed0>
2022-10-25 03:20:28 -0700

    new rs var sets

---
+++ parameters/rf_rs.py
@@ -0,0 +1,22 @@
+# Model: RF (Random Search)
+# Expl. variables: RS
+
+params = {
+    'name': 'RF-RS',
+    'desc': 'Random Forest (Random Search, default parameters) with RS data set',
+    'n_folds_cv': 5,
+    'n_folds_tuning': 5,
+    'random_state': 26,
+    'variable_set': 'rs',
+    'model': 'random_forest',
+    'model_params': {
+        'n_estimators': [100, 200, 400, 800, 1600],
+        'max_features': 15,
+        'min_samples_leaf': [2, 5, 10],
+        'min_samples_split': [4, 8, 12, 16],
+        'max_depth': [5, 10, 20, None],
+        'n_iter': 125,
+        'n_cpus': 20,
+        'scoring': 'neg_root_mean_squared_error'
+    }
+}
